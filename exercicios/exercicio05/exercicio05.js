3d91c8e8afca697df4644c9cc1ddef446892cbd9
Eduardo Palricas Cardoso <99208121+eduardopalricas@users.noreply.github.com>
2022-03-10 15:26:24 +0000

    exercicio fim js

--- exercicios/exercicio05/exercicio05.js
+++ exercicios/exercicio05/exercicio05.js
@@ -1,16 +1,57 @@
-var txtn = document.getElementById('txtnum')
-var num = Number(txtn.value)
-var sel = document.getElementById('selnum')
-var res = document.getElementById
-('res')
-function adicionar(){
-    if(txtn.value.length == 0){
-        window.alert('Número não digitado. Corrija os dados.')
-    }else if(num<1 || num>100){
-        window.alert('O numero digitado não se encontra no intervalo pedido. Corrija os seus dados.')
-    }else{
+let valores =[]
+let num = document.getElementById('txtnum')
+let lista = document.getElementById('selnum')
+let res = document.getElementById('res')
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
     }
 }
-function finalizar(){
 
+function inlista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
 }
+
+function adicionar(){
+    if(isNumero(num.value) && !inlista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        item.value = num.value
+        lista.appendChild(item)
+        res.innerHTML =''
+    } else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if(valores.length == 0){
+    window.alert('Adicione dados antes de finalizar')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos]>maior){
+                maior= valores[pos]
+            }else if(valores[pos]< menor){
+                menor = valores[pos]
+            }}
+        let media = soma/valores.length 
+        res.innerHTML =''
+        res.innerHTML =`<p>Ao todo temos ${tot} elementos.</p>` 
+        res.innerHTML += `<p>O maior número dentro destes é  ${maior} </p>`
+        res.innerHTML += `<p>O menor número dentro destes é o ${menor}</p>`
+        res.innerHTML += `<p> A soma destes elementos é ${soma}</p>` 
+        res.innerHTML+= `<p> A média destes valores é ${media} </p>`}}
